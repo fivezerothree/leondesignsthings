c316c79c9c11226f431dff250955bcf2a049584b
Leon Roy <leon@melee-media.co.uk>
2013-01-13 23:02:17 +0000

    First Commit
    Currently working with all browsers with the exception of Safari in
    combination Skrollr.js – Waiting on Prinzhorn to find new, better ways
    of prefix detection.
    https://github.com/Prinzhorn/skrollr/issues/121

---
+++ js/plugins.js
@@ -0,0 +1,152 @@
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+    var method;
+    var noop = function noop() {};
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+
+    while (length--) {
+        method = methods[length];
+
+        // Only stub undefined methods.
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+}());
+
+
+
+
+
+
+$(function(){
+	$("#logoContain a").hover(function(){
+		$("img", this).stop().animate({top:"-35px"},{queue:false,duration:200});
+	}, function() {
+		$("img", this).stop().animate({top:"0px"},{queue:false,duration:200});
+	});
+});
+
+
+
+jQuery(document).ready(function($) {
+ 
+	$(".scroll").click(function(event){		
+		event.preventDefault();
+		$('html,body').animate({scrollTop:$(this.hash).offset().top}, 1000);
+	});
+});
+
+
+
+
+$(function () {
+  $('.bubbleInfo').each(function () {
+    // options
+    var distance = 20;
+    var time = 250;
+    var hideDelay = 500;
+
+    var hideDelayTimer = null;
+
+    // tracker
+    var beingShown = false;
+    var shown = false;
+    
+    var trigger = $('.trigger', this);
+    var popup = $('#popup', this).css('opacity', 0);
+
+    // set the mouseover and mouseout on both element
+    $([trigger.get(0), popup.get(0)]).mouseover(function () {
+      // stops the hide event if we move from the trigger to the popup element
+      if (hideDelayTimer) clearTimeout(hideDelayTimer);
+
+      // don't trigger the animation again if we're being shown, or already visible
+      if (beingShown || shown) {
+        return;
+      } else {
+        beingShown = true;
+
+        // reset position of popup box
+        popup.css({
+          top: -265,
+          left: 10,
+          display: 'block' // brings the popup back in to view
+        })
+
+        // (we're using chaining on the popup) now animate it's opacity and position
+        .animate({
+          top: '-=' + distance + 'px',
+          opacity: 1
+        }, time, 'swing', function() {
+          // once the animation is complete, set the tracker variables
+          beingShown = false;
+          shown = true;
+        });
+      }
+    }).mouseout(function () {
+      // reset the timer if we get fired again - avoids double animations
+      if (hideDelayTimer) clearTimeout(hideDelayTimer);
+      
+      // store the timer so that it can be cleared in the mouseover if required
+      hideDelayTimer = setTimeout(function () {
+        hideDelayTimer = null;
+        popup.animate({
+          top: '-=' + distance + 'px',
+          opacity: 0
+        }, time, 'swing', function () {
+          // once the animate is complete, set the tracker variables
+          shown = false;
+          // hide the popup entirely after the effect (opacity alone doesn't do the job)
+          popup.css('display', 'none');
+        });
+      }, hideDelay);
+    });
+  });
+});
+
+  // Start of rotating jquery
+
+var img = $('.rotate');
+if(img.length > 0){
+    var offset = img.offset();
+    function mouse(evt){
+        var center_x = (offset.left) + (img.width()/2);
+        var center_y = (offset.top) + (img.height()/2);
+        var mouse_x = evt.pageX; var mouse_y = evt.pageY;
+        var radians = Math.atan2(mouse_x - center_x, mouse_y - center_y);
+        var degree = (radians * (180 / Math.PI) * -1) + 90;
+        img.css('-moz-transform', 'rotate('+degree+'deg)');
+        img.css('-webkit-transform', 'rotate('+degree+'deg)');
+        img.css('-o-transform', 'rotate('+degree+'deg)');
+        img.css('-ms-transform', 'rotate('+degree+'deg)');
+    }
+    $(document).mousemove(mouse);
+}
+
+// Start of main navigation fade
+
+$(document).ready(function(){ 
+
+  //Set the anchor link opacity to 0 and begin hover function
+  $("ul#nav-buttons li a").css({"opacity" : 0}).hover(function(){ 
+
+    //Fade to an opacity of 1 at a speed of 200ms
+    $(this).stop().animate({"opacity" : 1}, 500); 
+  
+    //On mouse-off
+    }, function(){
+
+    //Fade to an opacity of 0 at a speed of 100ms
+    $(this).stop().animate({"opacity" : 0}, 100); 
+  
+  });
+  
+});
